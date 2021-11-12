8034bbc1ae515a16a4e0d96405e3afa9b4525134
THCVP <88347509+THCVP@users.noreply.github.com>
2021-11-12 05:06:04 -0700

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,19 @@
+function text(content) 
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.5,0.9)
+end
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(1)
+        local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*2.2369)
+        -- Condition to check if the ped is in a vehicle
+        if(IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            text(math.floor(speed))
+        end
+    end
+end)
