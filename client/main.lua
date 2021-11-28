1fbe9822afd79ed028a3565026bacf3afff3ca03
THC <88347509+THCVP@users.noreply.github.com>
2021-11-27 21:00:02 -0800

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,1 +1,18 @@
+function text(content) 
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.5,0.9)
+end
+Citizen.CreateThread(function()
 
+    while true do
+        Citizen.Wait(1)
+        local speed = (GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*2.2369)
+        if(IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            text(math.floor(speed))
+        end
+    end
+end)
